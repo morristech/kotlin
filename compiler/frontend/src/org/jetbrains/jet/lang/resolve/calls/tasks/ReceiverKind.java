241b0dfbfff7835f5703ff24194204d3c797bbb7
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2013-12-10 14:08:12 +0400

    comment about ReceiverKind added

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/calls/tasks/ReceiverKind.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/calls/tasks/ReceiverKind.java
@@ -17,11 +17,27 @@
 package org.jetbrains.jet.lang.resolve.calls.tasks;
 
 public enum ReceiverKind {
+    // Expected receiver kind can be found for callable descriptor by CallableDescriptorUtil.getExpectedReceiverKind.
+    // It means the kind of the receiver the function (or property) expects.
+
+    // For each resolved call explicit receiver kind is stored.
+    // It often isn't the same as expected receiver kind for the corresponding function.
+
+    // For example: function foo in 'class A { fun B.foo() }' has expected receiver kind BOTH_RECEIVERS.
+    // It means both this object (of type A) and receiver argument (of type B) should be present in the corresponding resolved call.
+
+    // However for the invocation: 'with(a) { with(b) { foo() }}' explicit receiver kind is NO_EXPLICIT_RECEIVER,
+    // 'this object' refers to 'a', 'receiver argument' refers to 'b'.
+
+    // See JetResolvedCallsTestGenerated for more examples.
+
     RECEIVER_ARGUMENT,
     THIS_OBJECT,
     NO_EXPLICIT_RECEIVER,
 
-    // A very special case.
+    // The callable descriptor expects both receivers if it's an extension member in a class.
+
+    // The resolved call has both receivers explicitly in a very special case for 'invoke'.
     // In a call 'b.foo(1)' where class 'Foo' has an extension member 'fun B.invoke(Int)' function 'invoke' has two explicit receivers:
     // 'b' (as receiver argument) and 'foo' (as this object).
     BOTH_RECEIVERS;
